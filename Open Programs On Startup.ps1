08b82669a505f4365df538b4c382f8b6f53d0397
Ahmed Sayed <therealahme@gmail.com>
2024-03-30 15:47:58 +0200

    programs that i usually opens when i open my pc

---
+++ Open Programs On Startup.ps1
@@ -0,0 +1,14 @@
+# Open Visual Studio Code
+# Start-Process "C:\Users\elm4m\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+# Open Brave Browser
+Start-Process "brave"
+
+# Open Google Chrome
+Start-Process "chrome"
+
+# Open Slack
+Start-Process "C:\Users\elm4m\AppData\Local\slack\slack.exe"
+
+# Open MongoDB Compass
+Start-Process "C:\Users\elm4m\AppData\Local\MongoDBCompass\MongoDBCompass.exe"
