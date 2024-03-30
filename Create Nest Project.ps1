bae243633c8be1c21f5075e7f22f626f120ae000
Ahmed Sayed <therealahme@gmail.com>
2024-03-30 16:53:31 +0200

    write some confiramtion messages for the user

--- Create Nest Project.ps1
+++ Create Nest Project.ps1
@@ -6,15 +6,19 @@ $projectName = Read-Host "Enter your NestJS project name"
 
 # Change to the specified project location
 Set-Location $projectLocation
+Write-Output "Changed to project location: $projectLocation"
 
 # Create a new NestJS app in the specified location
 nest new $projectName
+Write-Output "Created Nest.js project: $projectName"
 
 # Navigate into the project directory
 Set-Location $projectName
+Write-Output "Changed to project directory: $projectName"
 
 # Open Visual Studio Code in the project directory
 code .
+Write-Output "Opened Visual Studio Code in project directory"
 
 # Modify tsconfig.json
 $tsconfigContent = @"
@@ -42,6 +46,8 @@ $tsconfigContent = @"
 }
 "@
 Set-Content -Path "tsconfig.json" -Value $tsconfigContent
+Write-Output "Modified tsconfig.json"
 
-#Install the most common packages that i use
+# Install the most common packages that i use
 npm i @nestjs/swagger class-validator class-transformer
+Write-Output "Installed common packages"
